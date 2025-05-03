afda1893b0fca222ed0a840d4c75af2dee3198d5
autofollower350 <followerauto350@gmail.com>
2025-05-04 01:32:11 +0530

    Update vars.py

--- vars.py
+++ vars.py
@@ -5,5 +5,5 @@
 from os import environ
 
 API_ID = int(environ.get("API_ID", "28590286"))
-API_HASH = environ.get("API_HASH", "6a68cc6b41219dc57b7a52914032f92f)
+API_HASH = environ.get("API_HASH", "6a68cc6b41219dc57b7a52914032f92f")
 BOT_TOKEN = environ.get("BOT_TOKEN", "7412939071:AAFk6_MJziu22UnZOv9crH-HcwLDqzQ1SVg")
